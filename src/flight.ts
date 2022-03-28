a6df0a196c8e4b0fee3fc4525b61be0b0c0ca5a9
Xavier Olive <git@xoolive.org>
2022-03-28 22:13:28 +0200

    rollup and feature methods with list of properties

--- src/flight.ts
+++ src/flight.ts
@@ -4,11 +4,16 @@ import { from, fromArrayBuffer, fromArrow, fromJSON, fromURL } from './data';
 import { make_date, timelike } from './time';
 
 import Table from 'arquero/dist/types/table/table';
+import { Op } from 'arquero/dist/types/op/op';
 
 interface Entry {
   latitude: number;
   longitude: number;
   timestamp: Date;
+}
+
+interface RollupObj {
+  [key: string]: string | Function | Op;
 }
 
 class Flight {
@@ -34,20 +39,50 @@ class Flight {
     return new Flight(await fromURL(url));
   }
 
-  feature = () =>
+  entries = () => Array.from(this.data) as Array<Entry>;
+
+  feature = (spec: RollupObj = {}) =>
     new Object({
       type: 'Feature',
       geometry: {
         type: 'LineString',
-        coordinates: (Array(...this.data) as Array<Entry>).map((elt) => [
-          elt.longitude,
-          elt.latitude,
-        ]),
+        coordinates: this.entries().map((elt) => [elt.longitude, elt.latitude]),
       },
+      properties: this.rollup(spec),
     });
+
+  rollup = (spec: RollupObj = {}) => {
+    return Object.fromEntries(
+      Object.entries(spec).map((key_values) => {
+        const [key, value] = key_values;
+        if (typeof value === 'string') {
+          // case: "start" (property) or "anything" for flight.anything()
+          const result: any = (this as { [key: string]: any })[value];
+          if (typeof result === 'function') {
+            return [key, result()];
+          } else {
+            return [key, result];
+          }
+        }
+        if (typeof value === 'function') {
+          // case: (flight) => flight.anything()
+          return [key, value(this)];
+        }
+        if (value?.toObject()?.func) {
+          // case: aq.op
+          const obj = this.data.rollup({ value }).object();
+          return [key, (obj as { value: any }).value];
+        }
+        return [key, undefined];
+      })
+    );
+  };
 
   min = (feature: string) => agg(this.data, op.min(feature));
   max = (feature: string) => agg(this.data, op.max(feature));
+  mean = (feature: string) => agg(this.data, op.mean(feature));
+  median = (feature: string) => agg(this.data, op.median(feature));
+  stdev = (feature: string) => agg(this.data, op.stdev(feature));
 
   get start(): Date {
     return this.min('timestamp');
